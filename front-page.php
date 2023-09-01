2ff51e6397dd220e3e8a42998f4bbd1814ccfe73
davidquintr <davidquintr97@gmail.com>
2023-09-01 07:53:30 -0600

    basic structure

---
+++ front-page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing WordPress</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" >
+    <link rel="stylesheet" href="/wp-content/themes/improvedtheme/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+
+    <header class="sticky top-0 border-b-2 bg-[#ffffff33] z-20 py-4 flex gap-2 backdrop-blur-[4px]">
+        <div class="max-w-[90rem] mx-auto w-screen px-2">
+            <div class="flex gap-2 items-center">
+                <h3 class="text-2xl text-[#454b54] font-bold">Testing WordPress</h3>
+            </div>
+        </div>
+    </header>
+    
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+function improvedtheme_register_styles(){
+
+    wp_enqueue_style('tailwind-css','');
+
+}
+
+
+?>
